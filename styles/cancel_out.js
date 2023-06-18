e9bde94f5b8575e725d942b77ef84097589426b6
Apollinaria Chernikova <74719407+ApollyCh@users.noreply.github.com>
2023-06-18 14:20:17 +0300

    Delete cancel_out.js

--- styles/cancel_out.js
+++
@@ -1,8 +0,0 @@
-const vw = Math.max(document.documentElement.clientWidth || 0, window.innerWidth || 0);
-const vh = Math.max(document.documentElement.clientHeight || 0, window.innerHeight || 0);
-
-let btn1 = document.getElementById('num1');
-let btn2 = document.getElementById('num2');
-
-btn1.style.left= `${vw /2 - 12 - 148}px`;
-btn2.style.left=`${vw/2 + 12}px`;
